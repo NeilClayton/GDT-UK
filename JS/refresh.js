ce6fea70041eec0f99f328a4b6429a9d29948079
Dominic Hart <dom_h@live.co.uk>
2016-06-19 19:17:53 +0100

    Changed delete windows
    Changed delete windows to match edit windows

--- JS/refresh.js
+++ JS/refresh.js
@@ -17,13 +17,28 @@ function overlay_b() {
     el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 }
 
+function overlay_b2() {
+    el = document.getElementById("overlay-b2");
+    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
+
 function overlay_u() {
     el = document.getElementById("overlay-u");
     el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 }
 
+function overlay_u2() {
+    el = document.getElementById("overlay-u2");
+    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
+
 function overlay_v() {
     el = document.getElementById("overlay-v");
+    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
+
+function overlay_v2() {
+    el = document.getElementById("overlay-v2");
     el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 }
 
